0343b9b04d52bdf0b017ff6ecc5637bcebe426f8
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-11-15 16:35:25 +0300

    Ajoute image dans error not_found

--- front/src/views/error/index.jsx
+++ front/src/views/error/index.jsx
@@ -1,12 +1,11 @@
 import {Link} from 'react-router-dom';
 import './error.css';
+import bg_error from '../../assets/images/error_bg.png';
 
 function Error() {
   return (
     <div className="container_error">
-        <h1>
-          Error page 404 Not found
-        </h1>
+        <img src={bg_error} alt="404 not found" />
         <p>Revenir à la page d'accueil...</p>
         <Link to="/">
           <button>Accueil</button>
