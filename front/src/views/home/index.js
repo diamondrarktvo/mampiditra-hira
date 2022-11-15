7e1e395d80774dd22c8d4359ce142d39c8c87a85
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-11-15 15:29:26 +0300

    Mise en place landing page presque finis

--- front/src/views/home/index.js
+++ front/src/views/home/index.js
@@ -1,24 +1,27 @@
-import logo from '../../assets/images/logo.png';
 import './home.css';
 
 function Home() {
   return (
     <div className="Home">
-      <div>
-          <img src={logo} className="Home-logo" alt="logo" style={{borderRadius: "150px"}} />
-          <p>
-            Edit <code>src/Home.js</code> and save to reload F5.
-          </p>
-          <p>{process.env.REACT_APP_MY_ENV_VARIABLE}</p>
-          <a
-            className="Home-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn mp3 gasy
-          </a>
-      </div>
+        <div className="landing">
+            <div className="content_landing">
+              <h2>MP3 DOWNLOADER GASY</h2>
+              <h4>LoremAdipisicing aliquip ullamco consectetur non id reprehenderit eiusmod proident Lorem. 
+                Eu id id pariatur aliqua dolore consequat consectetur enim aliquip esse ullamco aute nostrud 
+                fugiat. Magna deserunt ex sint id qui culpa ad. Pariatur tempor ullamco dolor est aliqua do 
+                esse voluptate proident adipisicing tempor. Esse sunt id nulla id cillum ad commodo aute labore. 
+                Adipisicing do culpa consequat eu enim. Cupidatat laboris sit sunt incididunt anim esse voluptate
+                dolor adipisicing nostrud velit culpa sit nulla.</h4>
+              <div className="button_utils">
+                  <button>
+                      Get started
+                  </button>
+                  <button>
+                      More info
+                  </button>
+              </div>
+            </div>
+        </div>
     </div>
   );
 }
