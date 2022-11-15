3e9718aacbf85e1c79a8f6ae62c4299f4304c649
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-11-15 16:22:24 +0300

    Amélioration page d'erreur

--- front/src/views/error/index.jsx
+++ front/src/views/error/index.jsx
@@ -1,13 +1,16 @@
+import {Link} from 'react-router-dom';
 import './error.css';
 
 function Error() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+    <div className="container_error">
+        <h1>
           Error page 404 Not found
-        </p>
-      </header>
+        </h1>
+        <p>Revenir à la page d'accueil...</p>
+        <Link to="/">
+          <button>Accueil</button>
+        </Link>
     </div>
   );
 }
