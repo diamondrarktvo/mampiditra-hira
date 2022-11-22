63a8469d6d2e3c05ea946df8519100b9eba8ed3c
diamondrarktvo <diamondra.rktvo@gmail.com>
2022-11-22 10:02:26 +0300

    Ajout font awesome dans le projet

--- front/src/index.js
+++ front/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Navigation from './views/Navigation';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ front/src/views/download/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Download (){
+    return (
+        <div>Welcome to view Download</div>
+    )
+}
+
+export default Download;
